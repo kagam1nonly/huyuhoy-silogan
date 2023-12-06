61843d97321c79c36e0fe2a5bd9555d4e6f676d7
James Piastro <78834746+kagam1nonly@users.noreply.github.com>
2023-12-07 06:36:14 +0800

    Deploying Huyuhoy Silogan using Heroku

--- huyuhoy/huyuhoy/settings.py
+++ huyuhoy/huyuhoy/settings.py
@@ -150,6 +150,8 @@ STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'), )
 django_heroku.settings(locals())
+
+DISABLE_COLLECTSTATIC = os.environ.get('DISABLE_COLLECTSTATIC', '0') == '1'
  
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
--- huyuhoy/huyuhoy/wsgi.py
+++ huyuhoy/huyuhoy/wsgi.py
@@ -1,16 +1,6 @@
-"""
-WSGI config for huyuhoy project.
-
-It exposes the WSGI callable as a module-level variable named ``application``.
-
-For more information on this file, see
-https://docs.djangoproject.com/en/4.2/howto/deployment/wsgi/
-"""
-
-import os
-
+# wsgi.py
 from django.core.wsgi import get_wsgi_application
-
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'huyuhoy.settings')
+from whitenoise import WhiteNoise
 
 application = get_wsgi_application()
+application = WhiteNoise(application)
