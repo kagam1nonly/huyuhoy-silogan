78307fb0a1afec1d0a7e1909b983962082fc0645
James Piastro <78834746+kagam1nonly@users.noreply.github.com>
2025-12-01 20:11:42 +0800

    Add localStorage migration to convert old media paths to static paths

--- huyuhoy/food/static/food/js/cart.js
+++ huyuhoy/food/static/food/js/cart.js
@@ -6,8 +6,48 @@ var price = document.querySelector('#meal-price')
 var rmv = document.querySelector('#rmv')
 var bill = document.querySelector('#ototal')
 
-//mealImage.innerHTML = '<img src="' + item.mealImageURL + '" alt="' + item.name + '" width="125" height="125">';
-//'<button class="remove-button" onclick="removeMeal(' + i + ')">Remove</button>';
+// Migrate old localStorage paths from /media/meal_images/ to /static/food/imgs/
+function migrateOldImagePaths() {
+    var migrated = false;
+    cartItems.forEach(function(item) {
+        if (item.mealImageURL && item.mealImageURL.includes('/media/meal_images/')) {
+            // Extract filename and version hash (e.g., "quarterpound_omqwEif.png" from "quarterpound_omqwEif.png")
+            var parts = item.mealImageURL.split('/');
+            var filename = parts[parts.length - 1];
+            var baseFilename = filename.split('_')[0] + '.png'; // Remove version hash suffix
+            
+            // Map old filenames to new ones
+            var fileMap = {
+                'quarterpound_': 'quarterpound.png',
+                'porksilog_': 'Porksilog.png',
+                'bangsilog_': 'bangsilog.png',
+                'cornsilog_': 'cornsilog.png',
+                'tunasilog_': 'tunasilog.png',
+                'spamsilog_': 'spamsilog.png',
+                'sisilog_': 'sisilog.png',
+                'porksteak_': 'porksteak.png'
+            };
+            
+            // Find matching filename
+            for (var key in fileMap) {
+                if (filename.startsWith(key)) {
+                    item.mealImageURL = '/static/food/imgs/' + fileMap[key];
+                    migrated = true;
+                    break;
+                }
+            }
+        }
+    });
+    
+    // Save migrated data back to localStorage
+    if (migrated) {
+        localStorage.setItem('orders', JSON.stringify(cartItems));
+    }
+}
+
+// Run migration when page loads
+migrateOldImagePaths();
+
 
 function displayCart() {
     var total = localStorage.getItem('total');
--- huyuhoy/food/static/food/js/order.js
+++ huyuhoy/food/static/food/js/order.js
@@ -3,7 +3,47 @@ var total = localStorage.getItem('total');
 var mcart = document.querySelector('#mcart');
 var mtotal = document.querySelector('#mtotal');
 
-// ADD MEAL 
+// Migrate old localStorage paths from /media/meal_images/ to /static/food/imgs/
+function migrateOldImagePaths() {
+    var migrated = false;
+    cartItems.forEach(function(item) {
+        if (item.mealImageURL && item.mealImageURL.includes('/media/meal_images/')) {
+            // Extract filename and version hash (e.g., "quarterpound_omqwEif.png" from "quarterpound_omqwEif.png")
+            var parts = item.mealImageURL.split('/');
+            var filename = parts[parts.length - 1];
+            var baseFilename = filename.split('_')[0] + '.png'; // Remove version hash suffix
+            
+            // Map old filenames to new ones
+            var fileMap = {
+                'quarterpound_': 'quarterpound.png',
+                'porksilog_': 'Porksilog.png',
+                'bangsilog_': 'bangsilog.png',
+                'cornsilog_': 'cornsilog.png',
+                'tunasilog_': 'tunasilog.png',
+                'spamsilog_': 'spamsilog.png',
+                'sisilog_': 'sisilog.png',
+                'porksteak_': 'porksteak.png'
+            };
+            
+            // Find matching filename
+            for (var key in fileMap) {
+                if (filename.startsWith(key)) {
+                    item.mealImageURL = '/static/food/imgs/' + fileMap[key];
+                    migrated = true;
+                    break;
+                }
+            }
+        }
+    });
+    
+    // Save migrated data back to localStorage
+    if (migrated) {
+        localStorage.setItem('orders', JSON.stringify(cartItems));
+    }
+}
+
+// Run migration when page loads
+migrateOldImagePaths(); 
 function addMeal(mid, mealName, mealImageUrl, withRice, withOutRice) {
     var radio = 'mealOption' + mid;
     var selectedRadio = document.querySelector('input[name="' + radio + '"]:checked');
