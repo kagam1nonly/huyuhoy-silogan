9e44b834c7fbea96b9d296de034d996f4ec6f6f7
James Piastro <78834746+kagam1nonly@users.noreply.github.com>
2026-03-23 09:15:01 +0800

    Add migration to create superuser with predefined credentials

---
+++ backend/food/migrations/0003_create_superuser.py
@@ -0,0 +1,19 @@
+from django.db import migrations
+from django.contrib.auth import get_user_model
+
+def create_superuser(apps, schema_editor):
+    User = get_user_model()
+    if not User.objects.filter(username='kagamipro').exists():
+        User.objects.create_superuser(
+            username='kagamipro',
+            email='jamespiastro20@gmail.com', # Use any email
+            password='zetsu123'
+        )
+
+class Migration(migrations.Migration):
+    dependencies = [
+        ('food', '0001_initial'), # Make sure this matches your previous migration file
+    ]
+    operations = [
+        migrations.RunPython(create_superuser),
+    ]
